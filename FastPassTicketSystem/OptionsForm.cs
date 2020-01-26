13ea04b8255846bfa49fd6ff823249fb4008f6f6
Karlo Paragas <karloparagas@gmail.com>
2020-01-26 09:14:17 -0800

    OK button is functional

--- FastPassTicketSystem/OptionsForm.cs
+++ FastPassTicketSystem/OptionsForm.cs
@@ -16,5 +16,10 @@ namespace FastPassTicketSystem
         {
             InitializeComponent();
         }
+
+        private void OkBtn_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.OK;
+        }
     }
 }
