719224d1e9475ad517ca9255a0ddc84c0db7151f
Karlo Paragas <karloparagas@gmail.com>
2020-01-25 19:03:25 -0800

    Created tickets form design

--- FastPassTicketSystem/Form1.Designer.cs
+++ FastPassTicketSystem/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace FastPassTicketSystem
 {
-    partial class Form1
+    partial class TitleBar
     {
         /// <summary>
         /// Required designer variable.
@@ -28,20 +28,167 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.OptionsBtn = new System.Windows.Forms.Button();
+            this.ExitBtn = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.IssueTicketBtn = new System.Windows.Forms.Button();
+            this.TitleBarTime = new System.Windows.Forms.Timer(this.components);
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
-            // Form1
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 31;
+            this.listBox1.Location = new System.Drawing.Point(34, 627);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(725, 376);
+            this.listBox1.TabIndex = 0;
+            // 
+            // OptionsBtn
+            // 
+            this.OptionsBtn.Location = new System.Drawing.Point(34, 1036);
+            this.OptionsBtn.Name = "OptionsBtn";
+            this.OptionsBtn.Size = new System.Drawing.Size(260, 55);
+            this.OptionsBtn.TabIndex = 1;
+            this.OptionsBtn.Text = "Options";
+            this.OptionsBtn.UseVisualStyleBackColor = true;
+            // 
+            // ExitBtn
+            // 
+            this.ExitBtn.Location = new System.Drawing.Point(499, 1036);
+            this.ExitBtn.Name = "ExitBtn";
+            this.ExitBtn.Size = new System.Drawing.Size(260, 55);
+            this.ExitBtn.TabIndex = 2;
+            this.ExitBtn.Text = "Exit";
+            this.ExitBtn.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(34, 26);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(725, 186);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Guests with the following tickets may now enter:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 83);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 32);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "0";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Location = new System.Drawing.Point(34, 235);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(728, 287);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Ticket Availability";
+            // 
+            // label6
+            // 
+            this.label6.Location = new System.Drawing.Point(374, 175);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(122, 38);
+            this.label6.TabIndex = 3;
+            this.label6.Text = "0";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label5
+            // 
+            this.label5.Location = new System.Drawing.Point(374, 78);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(122, 38);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "0";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(16, 181);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(273, 32);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Next available entry:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(331, 32);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Total tickets outstanding:";
+            // 
+            // IssueTicketBtn
+            // 
+            this.IssueTicketBtn.Location = new System.Drawing.Point(34, 544);
+            this.IssueTicketBtn.Name = "IssueTicketBtn";
+            this.IssueTicketBtn.Size = new System.Drawing.Size(299, 55);
+            this.IssueTicketBtn.TabIndex = 5;
+            this.IssueTicketBtn.Text = "Issue Ticket";
+            this.IssueTicketBtn.UseVisualStyleBackColor = true;
+            // 
+            // TitleBarTime
+            // 
+            this.TitleBarTime.Interval = 1000;
+            this.TitleBarTime.Tick += new System.EventHandler(this.TitleBarTime_Tick);
+            // 
+            // TitleBar
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1330, 927);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(796, 1103);
+            this.Controls.Add(this.IssueTicketBtn);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.ExitBtn);
+            this.Controls.Add(this.OptionsBtn);
+            this.Controls.Add(this.listBox1);
+            this.Name = "TitleBar";
+            this.Text = "Time Display";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button OptionsBtn;
+        private System.Windows.Forms.Button ExitBtn;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button IssueTicketBtn;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Timer TitleBarTime;
     }
 }
 
--- FastPassTicketSystem/Program.cs
+++ FastPassTicketSystem/Program.cs
@@ -16,7 +16,7 @@ namespace FastPassTicketSystem
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new TitleBar());
         }
     }
 }
