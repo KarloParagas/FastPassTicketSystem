29d28428bc8c23bdd022be7cf99da51871635e14
Karlo Paragas <karloparagas@gmail.com>
2020-01-26 18:35:42 -0800

    Added input validation

--- FastPassTicketSystem/OptionsForm.cs
+++ FastPassTicketSystem/OptionsForm.cs
@@ -19,7 +19,63 @@ namespace FastPassTicketSystem
 
         private void OkBtn_Click(object sender, EventArgs e)
         {
-            DialogResult = DialogResult.OK;
+            if (IsDataValid() == true) 
+            {
+                //Extract all input
+
+                //Assign it to the time slot class
+
+                //Store it in a collection
+
+                DialogResult = DialogResult.OK;            
+            }
+        }
+
+        private bool IsDataValid()
+        {
+            if (IsPresent() == true && IsInputValid() == true) 
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private bool IsInputValid()
+        {
+            //Variables to be used for testing, for valid integer and date time input
+            int validInteger;
+            DateTime validDateTime;
+
+            if (!DateTime.TryParse(StartTimeTxt.Text, out validDateTime) ||
+                !DateTime.TryParse(EndTimeTxt.Text, out validDateTime))  
+            {
+                MessageBox.Show("Start time and End time must be a valid date time input. Ex: (10:00 AM/PM)");
+                return false;
+            }
+            if (!int.TryParse(MinutesPerWindowTxt.Text, out validInteger) ||
+                !int.TryParse(GuestsPerWindowTxt.Text, out validInteger) ||
+                !int.TryParse(FirstTicketNumTxt.Text, out validInteger)) 
+            {
+                MessageBox.Show("Minutes per window, Guests per window, and " +
+                                "First ticket number must be a valid integer input.");
+                return false;
+            }
+
+            return true;
+        }
+
+        private bool IsPresent()
+        {
+            if (string.IsNullOrWhiteSpace(MinutesPerWindowTxt.Text) || 
+                string.IsNullOrWhiteSpace(GuestsPerWindowTxt.Text) ||
+                string.IsNullOrWhiteSpace(StartTimeTxt.Text) ||
+                string.IsNullOrWhiteSpace(EndTimeTxt.Text) ||
+                string.IsNullOrWhiteSpace(FirstTicketNumTxt.Text)) 
+            {
+                MessageBox.Show("Cannot submit an empty form");
+                return false;
+            }
+            return true;
         }
     }
 }
