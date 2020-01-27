f9a6a462eb6bc5964d0ead2780366826bf1a51e3
KarloParagas <karloparagas@gmail.com>
2020-01-26 21:15:53 -0800

    Edited TimeSlot class

--- FastPassTicketSystem/TimeSlot.cs
+++ FastPassTicketSystem/TimeSlot.cs
@@ -9,32 +9,35 @@ namespace FastPassTicketSystem
     /// <summary>
     /// A class that represents the time slot for assigning tickets
     /// </summary>
-    class TimeSlot
+    public class TimeSlot
     {
-        public DateTime _startTime;
-        public DateTime _endTime;
-        public TimeSpan _timeSlotLength;
-
-        public DateTime _time { get; set; }
-
         public int _numberOfTickets { get; set; }
 
+        /// <summary>
+        /// Obtains the current time
+        /// </summary>
+        public DateTime Time 
+        {
+            get { return Time; }
+            private set { Time = DateTime.Now; }
+        }
+
+        /// <summary>
+        /// Obtains the start time provided in the Options form
+        /// </summary>
         public DateTime StartTime
         {
-            get { return _startTime; }
-            set { _startTime = value; }
+            get { return StartTime; }
+            private set { StartTime = value; }
         }
 
-        public DateTime EndTime 
-        {
-            get { return _endTime; }
-            set { _endTime = value; }
-        }
-
+        /// <summary>
+        /// Calculates the time slot's length based on the current time, and the start time
+        /// </summary>
         public TimeSpan TimeSlotLength
         {
-            get { return _timeSlotLength; }
-            set { _timeSlotLength = _endTime - _startTime; }
+            get { return TimeSlotLength; }
+            private set { TimeSlotLength = Time - StartTime; }
         }
     }
 }
