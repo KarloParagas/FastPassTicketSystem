969217278c381cfa10ca036f3479cb2a12c5a70d
KarloParagas <karloparagas@gmail.com>
2020-02-04 13:51:02 -0800

    Added new migration with updated columns

---
+++ FastPassTicketSystem/Migrations/202002042130114_AddedCustomerPurchases.cs
@@ -0,0 +1,30 @@
+﻿namespace FastPassTicketSystem.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddedCustomerPurchases : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.CustomerPurchases",
+                c => new
+                    {
+                        CustomerId = c.Int(nullable: false, identity: true),
+                        FirstName = c.String(nullable: false, maxLength: 50),
+                        LastName = c.String(nullable: false, maxLength: 50),
+                        Email = c.String(nullable: false),
+                        PhoneNumber = c.String(nullable: false, maxLength: 10),
+                        DatePurchased = c.DateTime(nullable: false),
+                    })
+                .PrimaryKey(t => t.CustomerId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.CustomerPurchases");
+        }
+    }
+}
