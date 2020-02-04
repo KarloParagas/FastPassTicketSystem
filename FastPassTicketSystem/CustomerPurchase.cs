dd1145d4d276875e28745deb9a869b36bc5127d2
KarloParagas <karloparagas@gmail.com>
2020-02-03 18:56:22 -0800

    Added a class for customer ticket purchases

---
+++ FastPassTicketSystem/CustomerPurchase.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FastPassTicketSystem
+{
+    public class CustomerPurchase
+    {
+        [Key]
+        public int TicketNumber { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string LastName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(10)]
+        public string PhoneNumber { get; set; }
+    }
+}
