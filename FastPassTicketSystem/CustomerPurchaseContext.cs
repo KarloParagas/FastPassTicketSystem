93b4fb4cddc40abc5b6c6bac06f3b675eadaa14c
KarloParagas <karloparagas@gmail.com>
2020-02-03 18:55:16 -0800

    Implemented code first migration and database for ticket purchases

---
+++ FastPassTicketSystem/CustomerPurchaseContext.cs
@@ -0,0 +1,20 @@
+namespace FastPassTicketSystem
+{
+    using System;
+    using System.Data.Entity;
+    using System.Linq;
+
+    public class CustomerPurchaseContext : DbContext
+    {
+        public CustomerPurchaseContext()
+            : base("name=CustomerPurchaseContext")
+        {
+        }
+
+        // Add a DbSet for each entity type that you want to include in your model. For more information 
+        // on configuring and using a Code First model, see http://go.microsoft.com/fwlink/?LinkId=390109.
+
+        // public virtual DbSet<MyEntity> MyEntities { get; set; }
+        public virtual DbSet<CustomerPurchase> CustomerPurchases { get; set; }
+    }
+}
---
+++ FastPassTicketSystem/Migrations/202002040247519_AddedCustomerPurchases.cs
@@ -0,0 +1,29 @@
+﻿namespace FastPassTicketSystem.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddedCustomerPurchases : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.CustomerPurchases",
+                c => new
+                    {
+                        TicketNumber = c.Int(nullable: false, identity: true),
+                        FirstName = c.String(nullable: false, maxLength: 50),
+                        LastName = c.String(nullable: false, maxLength: 50),
+                        Email = c.String(nullable: false),
+                        PhoneNumber = c.String(nullable: false, maxLength: 10),
+                    })
+                .PrimaryKey(t => t.TicketNumber);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.CustomerPurchases");
+        }
+    }
+}
