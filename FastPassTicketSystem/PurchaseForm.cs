6acfda4dcee191381a94a503443e2f16a3ff1fd9
KarloParagas <karloparagas@gmail.com>
2020-02-03 19:38:51 -0800

    Customer purchases gets added to the database

--- FastPassTicketSystem/PurchaseForm.cs
+++ FastPassTicketSystem/PurchaseForm.cs
@@ -20,6 +20,17 @@ namespace FastPassTicketSystem
         private void PurchaseTicket_Click(object sender, EventArgs e)
         {
             //Create a purchase object
+            CustomerPurchase cp = new CustomerPurchase() 
+            {
+                FirstName = FirstNameTxt.Text,
+                LastName = LastNameTxt.Text,
+                Email = EmailTxt.Text,
+                PhoneNumber = PhoneNumberTxt.Text
+            };
+
+            //Add the purchase to the database
+            CustomerPurchaseDb.Add(cp);
+            MessageBox.Show($"Ticket {cp.TicketNumber} successfully purchased");
         }
     }
 }
